8136411cdff2adae041aa9798ea06bb313a10841
Bekoavci1 <c1811006@student.cankaya.edu.tr>
2023-12-29 18:24:00 +0300

    son değişiklikler

--- src/pages/homepage/FeaturedPost.js
+++ src/pages/homepage/FeaturedPost.js
@@ -6,12 +6,13 @@ import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import { green, red } from '@mui/material/colors';
 
 function FeaturedPost(props) {
   const { post } = props;
 
   return (
-    <Grid item xs={12} md={6}>
+    <Grid item xs={12} md={12}> {/* Updated md size to 12 */}
       <CardActionArea component="a" href="#">
         <Card sx={{ display: 'flex' }}>
           <CardContent sx={{ flex: 1 }}>
@@ -24,15 +25,13 @@ function FeaturedPost(props) {
             <Typography variant="subtitle1" paragraph>
               {post.description}
             </Typography>
-            <Typography variant="subtitle1" color="primary">
-              Continue reading...
-            </Typography>
           </CardContent>
           <CardMedia
             component="img"
-            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+            sx={{ width: 160, display: { xs: 'none', sm:'block' } }}
             image={post.image}
             alt={post.imageLabel}
+          
           />
         </Card>
       </CardActionArea>
